a3d366aba6f2b6436b3b595535a9c6c3b8b1c6de
Yohaan <43923340+Yashas111@users.noreply.github.com>
2019-10-28 14:02:29 +0530

    Add files via upload

--- discussion_forum.sql
+++ discussion_forum.sql
@@ -1,9 +1,10 @@
 create database dforum;
 use dforum;
 
-create table user(
-	id int primary key,
+create table users(
+    email varchar(200) not null primary key,
     uname varchar(100),
-    email varchar(200) not null,
     password varchar(10) not null
 );
+
+select * from users;
