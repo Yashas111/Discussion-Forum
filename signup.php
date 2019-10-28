341ba26766ba2e929628bd665b6b5e3b34a671e2
KnightShade <=>
2019-10-28 23:41:57 +0530

    refined signup page

--- signup.php
+++ signup.php
@@ -1,20 +1,13 @@
 <?php
-$conn = mysqli_connect('localhost','KnightShade','dragon391','dforum');
+    $conn = mysqli_connect('localhost','KnightShade','dragon391','dforum');
     $email=$_POST['email'];
-    $uname=$_POST['user_name'];
+    $uname=$_POST['name'];
     $pass=$_POST['password'];
-    $cpass=$_POST['password_confirmation'];
-
-    if($pass == $cpass){
-        //print('Account created');
-        $q = "INSERT INTO users VALUES('$email','$uname','$pass')" ;
-        //$q = 'SELECT * from users';
-        echo '<h1 style="text-align:center">Please <a href="./login.html">login</a> to continue</h1>';
-        mysqli_query($conn, $q);
-    }else
-        echo '<h1 style="text-align:center">Please <a href="./signup.html">confirm</a> the password</h1>';
-    
-    mysqli_close($conn);
+    //print('Account created');
+    $q = "INSERT INTO users VALUES('$email','$uname','$pass')";
+    //$q = 'SELECT * from users';
+    mysqli_query($conn, $q);
+    echo "Done";
 ?>
 
 
