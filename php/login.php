7ff2d5adb62ee13e9f72cf429dc698ee6bb677a0
KnightShade <=>
2019-10-30 11:27:35 +0530

    modified login and signup

--- php/login.php
+++ php/login.php
@@ -11,8 +11,8 @@
     $pass = mysqli_fetch_assoc($log);
     //print($pass['password']);
     if($pass['password']==$password)
-        echo "Done";
+        echo "200|" . $pass["uname"];
     else
-        echo "Incorrect email or password!";
+        echo "201|Incorrect email or password!";
 
 ?>
--- php/signup.php
+++ php/signup.php
@@ -4,11 +4,21 @@
     $email=$_POST['email'];
     $uname=$_POST['name'];
     $pass=$_POST['password'];
-    //print('Account created');
-    $q = "INSERT INTO users VALUES('$email','$uname','$pass')";
-    //$q = 'SELECT * from users';
-    mysqli_query($conn, $q);
-    echo "Done";
+    $q1 = "SELECT * from users where email='$email' or uname='$uname'";
+    //print($q1);
+    $log = mysqli_query($conn, $q1);
+    //print($log);
+    $row = mysqli_fetch_assoc($log);
+
+    if($row) {
+        echo 201;
+    } else {
+        //print('Account created');
+        $q = "INSERT INTO users VALUES('$email','$uname','$pass')";
+        //$q = 'SELECT * from users';
+        mysqli_query($conn, $q);
+        echo 200;
+    }
 ?>
 
 
